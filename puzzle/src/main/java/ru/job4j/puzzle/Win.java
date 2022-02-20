a523c8a140eddb7efb97e1b9761eb863ad607c63
Corsair <corsair954@mail.ru>
2022-02-20 15:53:46 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,35 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
+        boolean rsl = false;
+        for (int i = 0; i != board.length; i++) {
+            if (board[i][i] == 1 && (isMonoRow(board,i) || isMonoColumn(board, i))) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean isMonoRow(int[][] board, int row) {
         boolean rsl = true;
+        for (int i = 0; i != board[row].length; i++) {
+            if (board[row][i] != 1){
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean isMonoColumn(int[][] board, int column) {
+        boolean rsl = true;
+        for (int i = 0; i != board.length; i++) {
+            if (board[i][column] != 1) {
+                rsl = false;
+                break;
+            }
+        }
         return rsl;
     }
 }
